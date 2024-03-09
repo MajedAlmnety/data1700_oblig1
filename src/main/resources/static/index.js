7762859d0bcafef62a15ff27b0d46dccbf214d18
Majed_07 <mm0443643@gmail.com>
2024-03-09 15:51:01 +0100

    marge...

---
+++ src/main/resources/static/index.js
@@ -0,0 +1,71 @@
+function submit(){
+    $("#antallError").text("");
+    $("#fornavnError").text("");
+    $("#etternavnError").text("");
+    $("#talError").text("");
+    $("#epostError").text("");
+
+    let billett={
+        film: $("#filmer").val(),
+        antall: $("#antall").val(),
+        fornavn: $("#fornavn").val(),
+        etternavn: $("#etternavn").val(),
+        tlf: $("#tel").val(),
+        epost: $("#epost").val()
+    }
+
+
+    if(billett.fornavn==="" || billett.etternavn===""|| billett.tlf==="" || billett.epost==="" ||Number(billett.antall)===0 || billett.film==="" ) {
+        if(Number(billett.antall)===0) {
+            $("#antallError").html("Må vare større enn null".fontcolor("red"));
+        }
+        if (billett.fornavn === "") {
+            $("#fornavnError").html("Må skrive noe inn i fornavn".fontcolor("red"));
+        }
+        if (billett.etternavn === "") {
+            $("#etternavnError").html("Må skrive noe i etternavn".fontcolor("red"));
+        }
+        if (billett.tlf === "") {
+            $("#talError").html("Må skrive noe i telefonnu".fontcolor("red"));
+        }
+        if (billett.epost === "") {
+            $("#epostError").html("Må skrive noe i epost".fontcolor("red"));
+        }
+    }else{
+
+        listfilmer();
+        $.post(" http://localhost:8000/save_data", billett, function(){
+            listfilmer();
+        });
+        $("#filmer").val("");
+        $("#fornavn").val("");
+        $("#etternavn").val("");
+        $("#antall").val(0);
+        $("#tel").val("");
+        $("#epost").val("");
+
+    }
+
+}
+
+function listfilmer() {
+    $.get( "http://localhost:8000/list_data", function( data ) {
+        formaterData(data);
+    });
+}
+
+function formaterData(billetter){
+    let ut = "<table class='table'><tr><th>Film</th><th>Antall</th><th>Fornavn</th><th>Etternavn</th><th>Telnr</th><th>Epost</th></tr>";
+    for (let billett of billetter){
+        ut+="<tr><td>"+billett.film+"</td><td>"+billett.antall+"</td><td>"+billett.fornavn+"</td>" +
+            "<td>"+billett.etternavn+"</td><td>"+billett.tlf+"</td><td>"+billett.epost+"</td></tr>";
+    }
+    ut+="</table>";
+    $("#billetter").html(ut);
+}
+
+function delete_alle(){
+    $.get( "http://localhost:8000/delete_data", function() {
+        listfilmer();
+    });
+}
